91e4627089c9e77f0bd9d5f8237c8cf401de2c9d
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2022-10-24 11:05:01 +0700

    Update profile.sh

--- xray/profile.sh
+++ xray/profile.sh
@@ -9,8 +9,11 @@ fi
 mesg n || true
 MYIP=$(curl -s ipv4.icanhazip.com)
 exp=$(curl -s https://raw.githubusercontent.com/Mahfud2128/access/main/ip | grep $MYIP | awk '{print$3}')
+limit=$(curl -s https://raw.githubusercontent.com/Mahfud2128/access/main/ip | grep $MYIP | awk '{print$5}')
+dmon=$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($4, 1, 1)}')
 name=$(curl -s https://raw.githubusercontent.com/Mahfud2128/access/main/ip | grep $MYIP | awk '{print$2}')
+pesan=$(curl -s https://raw.githubusercontent.com/Mahfud2128/access/main/ip | grep $MYIP | awk '{ print $6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20 }' )
 screen -r setup
 clear
 neofetch
-echo -e "Welcome To Autscript \"GDStore\"\nName: $name""\nExp Script: $exp""\nKetik Menu Untuk Melihat Daftar Perintah" | boxes -d cat | lolcat
+echo -e "Welcome To Autscript \"GDStore\"\n► Name: $name""\n► Exp Script: $exp""\n► Batas Bandwidth: $limit""\n► Bandwitch Saat ini: $dmon""\n► Pesan: $pesan""\nKetik Menu Untuk Melihat Daftar Perintah" | boxes -d cat | lolcat
